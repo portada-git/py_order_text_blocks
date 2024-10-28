3fb2c9a7cdfe9eb84129328b21fc320bf1f49b99
jcanell4 <jcanell4@xtec.cat>
2024-10-29 00:12:22 +0100

    Change stacked image creation for image blocks creation

--- py_portada_order_blocks/redraw_image.py
+++ py_portada_order_blocks/redraw_image.py
@@ -1,8 +1,12 @@
 import os
 import cv2
+
 from .reorder_image_from_arcanum_blocks import order_blocks, stack_blocks
 from .decrypt import decrypt_file_openssl
-from newspaper_segmentation_client import run_newspaper_segmentation
+from newspaper_segmentation_client import run_newspaper_segmentation_on_image
+from py_image_utils.image_utilities_cv import resize_image_percent_til_size, convert_ordered_block_stack_to_cv2
+from pathlib import Path
+from PIL import Image
 
 
 class PortadaRedrawImageForOcr(object):
@@ -15,6 +19,7 @@ class PortadaRedrawImageForOcr(object):
             self.image = None
             self._image_path = ''
         self.config = pconfig
+        self.image_blocks = []
 
     @property
     def image(self):
@@ -23,6 +28,7 @@ class PortadaRedrawImageForOcr(object):
     @image.setter
     def image(self, val):
         self._image = val
+        self.image_blocks = []
 
     @property
     def image_path(self):
@@ -63,16 +69,55 @@ class PortadaRedrawImageForOcr(object):
             image_path = self.image_path
         cv2.imwrite(image_path, self.image)
 
+    def save_block_images(self, dir_name="", image_name=""):
+        self.__verify_image()
+        for bi in self.image_blocks:
+            if len(image_name) == 0:
+                image_name = bi["file_name"]
+
+            if len(dir_name) > 0:
+                image_path = Path(dir_name).joinpath(Path(image_name).stem)
+            else:
+                image_path = Path(image_name)
+
+            with open("{file_name}_{count:03d}{extension}".format(file_name=image_path, count=bi["count"],
+                                                                   extension=bi["extension"]), "wb") as bf:
+                bf.write(bi["image"])
+
+    def pre_process_image(self, image, size, ext=""):
+        if len(ext)==0:
+            img, _, _, _ = resize_image_percent_til_size(image, size)
+        else:
+            img, _, _, _ = resize_image_percent_til_size(image, size, ext)
+        return img
+
     def process_image(self):
         self.__verify_image()
         # get json blocks from arcanum
-        arcanum_json = self.get_arcanum_blocks()
+        ext = Path(self.image_path).suffix
+        if len(ext) == 0:
+            ext = ".jpg"
+        image = self.pre_process_image(self.image, 6291456, ext)
+        arcanum_json = self.get_arcanum_blocks(image)
         corrected_blocks = order_blocks(self.image, arcanum_json)
+        # corrected_blocks = post_process(corrected_blocks, proc)
         # self.image = draw_numbered_blocks(corrected_blocks, self.image)
-        self.image = stack_blocks(self.image, corrected_blocks)
+        blocks = convert_ordered_block_stack_to_cv2(self.image, corrected_blocks)
+        count = 0
+        file_name = Path(self.image_path).stem
+        for block in blocks:
+            self.image_blocks.append(
+                dict(file_name=file_name, extension=ext, count=count, image=cv2.imencode(ext, block)[1]))
+            count = count + 1
 
-    def get_arcanum_blocks(self):
+    def get_arcanum_blocks(self, image=None):
         decrypt_key = os.environ['MUNACRA_TERCES']
         arcanum_key = decrypt_file_openssl(self._config['arcanum_key_path'], decrypt_key)
-        return run_newspaper_segmentation(self.image_path, arcanum_key)
+        if image is None:
+            image = self.image
 
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        pil_img = Image.fromarray(image)
+
+        return run_newspaper_segmentation_on_image(pil_img, arcanum_key)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='py_portada_order_blocks',
-    version='0.0.7',
+    version='0.0.8',
     description='tools for creating images of documents with only one column to avoid ordering problems when they are processed by OCR within the PortADa project',
     author='PortADa team',
     author_email='jcbportada@gmail.com',
@@ -19,7 +19,8 @@ setup(name='py_portada_order_blocks',
         'opencv-python >= 4.8,<4.9',
         'cryptography~=42.0.7',
         'numpy~=1.26.4',
-        'arcanum-newspaper-segmentation-client~=1.8.4'
+        'arcanum-newspaper-segmentation-client~=1.8.4',
+	'py_image_utils @ git+https://github.com/portada-git/py_image_utils#egg=py_image_utils'
     ], 
     python_requires='>=3.9',
     zip_safe=False)
